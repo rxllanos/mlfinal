2b7dd37d9939a7552b7db0b0c422da7109c8b259
rxllanos <rxllanos@gmail.com>
2023-06-29 15:04:31 -0500

    host name

--- finca/settings.py
+++ finca/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1',
-                 'https://lasmirlas.azurewebsites.net']
+                 'lasmirlas.azurewebsites.net']
 
 
 INSTALLED_APPS = [
